23f1680c42123fb99daeb8ce9c31bfd6d65c2fba
nds <nds@tomsksoft.com>
2019-03-01 13:59:41 +0700

    delete check range

--- vecap.h
+++ vecap.h
@@ -6,60 +6,65 @@
 
 namespace container {
 
-template <typename KeyType, typename ValueType>
-class Vecap {
+template<typename KeyType, typename ValueType>
+class Vecap
+{
  public:
   using iterator = typename std::vector<ValueType>::iterator;
   using const_iterator = typename std::vector<ValueType>::const_iterator;
 
   Vecap() = default;
 
-  Vecap(std::initializer_list<std::pair<KeyType, ValueType> > init) {
+  Vecap(std::initializer_list<std::pair<KeyType, ValueType> > init)
+  {
     for (const auto& item : init) {
       push_back(item);
     }
   }
 
-  ValueType& operator[](int pos) {
-    if (!check_range(pos)) throw std::range_error("Invalidate index");
+  ValueType& operator[](size_t pos)
+  {
     return values_[pos];
   }
 
-  const ValueType& operator[](int pos) const {
-    if (!check_range(pos)) throw std::range_error("Invalidate index");
+  const ValueType& operator[](size_t pos) const
+  {
     return values_[pos];
   }
 
-  void push_back(const std::pair<KeyType, ValueType>& item) {
+  void push_back(const std::pair<KeyType, ValueType>& item)
+  {
     indexes_[item.first] = values_.size();
     values_.push_back(item.second);
   }
 
-  bool check_range(int pos) const {
-    return pos >= 0 && pos < values_.size();
-  }
-
-  size_t size() const {
+  size_t size() const
+  {
     return values_.size();
   }
 
-  iterator begin() {
+  iterator begin()
+  {
     return values_.begin();
   }
 
-  const_iterator begin() const {
+  const_iterator begin() const
+  {
     return values_.begin();
   }
 
-  iterator end() {
+  iterator end()
+  {
     return values_.end();
   }
 
-  const_iterator end() const {
+  const_iterator end() const
+  {
     return values_.end();
   }
 
-  iterator find(const KeyType& key) {
+  iterator find(const KeyType& key)
+  {
     auto index = indexes_.find(key);
 
     if (index == indexes_.end()) {
@@ -69,7 +74,8 @@ class Vecap {
     return values_.begin() + index->second;
   }
 
-  const_iterator find(const KeyType& key) const {
+  const_iterator find(const KeyType& key) const
+  {
     auto index = indexes_.find(key);
 
     if (index == indexes_.end()) {
@@ -79,11 +85,9 @@ class Vecap {
     return values_.begin() + index->second;
   }
 
-  iterator erase(iterator pos) {
+  iterator erase(iterator pos)
+  {
     auto index = std::distance(values_.begin(), pos);
-    if (index >= values_.size() && index < 0) {
-      return pos;
-    }
 
     for (auto it = indexes_.begin(); it != indexes_.end();) {
       if (it->second == index) {
@@ -101,7 +105,7 @@ class Vecap {
 
  private:
   std::vector<ValueType> values_;
-  std::unordered_map<KeyType, int> indexes_;
+  std::unordered_map<KeyType, size_t> indexes_;
 };
 
 } // namespace container
